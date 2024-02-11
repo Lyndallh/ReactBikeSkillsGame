a78f0bbda6adf964b20c22449554fdd2fd50fed8
lindi <lindi.harding@gmail.com>
2024-02-11 19:56:51 +0800

    initial commit, new player, addpractice

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import { useState } from 'react';
+import PlayerStats from './components/NewPlayerForm';
+
+import './App.css'
+import NewPlayerForm from './components/NewPlayerForm';
+
+function App() {
+  const [playerStats, setPlayerStats] = useState([
+    {
+      id: 1,
+      name: 'Lindi',
+      practiceCount: 0,
+      passedPractie: false,
+      can_land_big_jump: false,
+      attempted_big_jump: false,
+    },
+  ]);
+
+  const addNewPlayer = (text) => {   // bring in the new input text into the function
+    const newPlayer = {
+      name: text,
+      practiceCount: 0,
+      passedPractie: false,
+      can_land_big_jump: false,
+      attempted_big_jump: false,
+    };
+    
+    const newPlayerStats = [...playerStats, newPlayer];    // the new array is the 'exsisting array' and the text that was inputted into the input feild
+    console.log(newPlayerStats);
+    setPlayerStats(newPlayerStats);    // update the todoitems state with the new array
+  };
+
+ // the addPractice function is passed to the PlayerStats component as a prop. It will be called when the button is clicked and it will update the practiceCount for the player with the given id by 1.
+  const addPractice = (index) => {
+    const newPlayerStats = [...playerStats];
+    newPlayerStats[index].practiceCount += 1;
+    setPlayerStats(newPlayerStats);
+  };
+
+  return (
+    <div className='app'>
+    <h1 className='bike-skills-game'>Bike Skills Game</h1>
+      {playerStats.map((player, index) => (
+        <div key={index}> 
+        {player.name} has practiced {player.practiceCount} times
+          <button onClick={() => addPractice(index)}>Add Practice</button>
+        </div>
+      ))}
+            <NewPlayerForm addNewPlayer={addNewPlayer} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NewPlayerForm/index.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+
+function NewPlayerForm(props) {
+    const [value, setValue] = useState("")
+
+    const handleSubmit = (event) => { //handleSubmit is a built in function that expects an event (the event is created from onSubmit) is often referred to as 'e' instead of event
+        event.preventDefault(); // we want to stop html and JS default behaviour
+        if (!value) return; // is there is not value, early return so that blank values don't go into the database
+        props.addNewPlayer(value); 
+        setValue('');
+    };
+
+    return (// could use onSubmit inside form properties like this: <form onSubmit={handleSubmit}> instead of having a onClick button but using a button is more user friendly
+        <form> 
+            <input 
+                type="text" 
+                placeholder="Add a new player"
+                value={value} // this is resetting the input feild to '' using SetValue('') from line 10
+                onChange={(event) => setValue(event.target.value)} //event.target is the input
+            />
+            <button onClick={handleSubmit}>Submit</button>
+        </form>  
+    );
+}
+
+export default NewPlayerForm;
